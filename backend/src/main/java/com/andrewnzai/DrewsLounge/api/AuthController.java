a47ea08ecd52e0724bfccce5e191a04f90b08e76
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-07-02 16:07:38 +0300

    Created signup functionality

--- backend/src/main/java/com/andrewnzai/DrewsLounge/api/AuthController.java
+++ backend/src/main/java/com/andrewnzai/DrewsLounge/api/AuthController.java
@@ -1,2 +1,29 @@
-package com.andrewnzai.DrewsLounge.api;public class AuthController {
+package com.andrewnzai.DrewsLounge.api;
+
+import com.andrewnzai.DrewsLounge.dtos.RegisterRequest;
+import com.andrewnzai.DrewsLounge.services.AuthService;
+import com.andrewnzai.DrewsLounge.utils.APIResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(name = "/api/auth/")
+@AllArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+    @PostMapping(name = "signup")
+    public APIResponse signup(@RequestBody RegisterRequest registerRequest) throws Exception {
+        authService.signup(registerRequest);
+
+        return APIResponse.builder()
+                .statusCode(200)
+                .isSuccessful(true)
+                .message("Successful signup")
+                .data(null)
+                .build();
+    }
 }
--- backend/src/main/java/com/andrewnzai/DrewsLounge/utils/APIResponse.java
+++ backend/src/main/java/com/andrewnzai/DrewsLounge/utils/APIResponse.java
@@ -1,2 +1,17 @@
-package com.andrewnzai.DrewsLounge.utils;public class APIResponse {
+package com.andrewnzai.DrewsLounge.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class APIResponse {
+    private String message;
+    private boolean isSuccessful;
+    private int statusCode;
+    private Object data;
 }
