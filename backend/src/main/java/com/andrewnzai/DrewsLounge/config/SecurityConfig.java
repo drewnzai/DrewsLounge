26ef019d2b33a96a2979f2618e1530e77b04d3d1
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-07-02 17:58:39 +0300

    Added exclusion for Swagger

--- backend/src/main/java/com/andrewnzai/DrewsLounge/config/SecurityConfig.java
+++ backend/src/main/java/com/andrewnzai/DrewsLounge/config/SecurityConfig.java
@@ -1,19 +1,12 @@
 package com.andrewnzai.DrewsLounge.config;
 
-import com.andrewnzai.DrewsLounge.services.UserDetailsServiceImpl;
 import com.andrewnzai.DrewsLounge.utils.JwtFilter;
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -25,7 +18,6 @@ import java.util.List;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
-@Slf4j
 @Configuration
 @EnableWebSecurity
 @AllArgsConstructor
@@ -41,6 +33,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request.requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/v3/api-docs/**").permitAll()
+                        .requestMatchers("/swagger-ui.html").permitAll()
                         .requestMatchers("/swagger-ui/**").permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
