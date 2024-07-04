a0dd763b07cf154644532dc6d862f046a06d48c3
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-07-04 19:37:24 +0300

    Modified service

--- frontend/src/services/AuthService.service.ts
+++ frontend/src/services/AuthService.service.ts
@@ -4,19 +4,18 @@ import { RegisterRequest } from "../models/RegisterRequest";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 
-const base_url = "http://localhost:8080/api/";
+const base_url = "http://localhost:8080/api/auth/";
 
 export default class AuthService{
     private navigate = useNavigate();
 
     signup(registerRequest: RegisterRequest){
-        axios.post(base_url + "auth/signup", registerRequest)
+        axios.post(base_url + "signup", registerRequest)
             .then(
                 (response) => {
                     if(response.status === 200){
                         toast.success("Signup succesful, check email for verification steps");
-                        this.navigate("/login");
-                        return response.data;
+                        this.navigate("/verifyaccount");
                     }
                     else if(response.status === 409){
                         toast.error("Signup unsuccessful, username already in use. Retry");
@@ -25,5 +24,28 @@ export default class AuthService{
                     return response;
                 }
             )
+    }
+
+    login(loginRequest: LoginRequest){
+        axios.post(base_url + "login", loginRequest)
+        .then(
+            (response) => {
+                
+            switch(response.data){
+                case "Wrong Credentials":
+                case "User does not exist":
+                case "Verify account":
+                    toast.error(response.data);
+                    break;
+
+                default:{
+                    localStorage.setItem("user", JSON.stringify(response.data));
+                    this.navigate("/");
+                    break;
+                    }
+}
+                return response;
+            }
+        )
     }
 }
