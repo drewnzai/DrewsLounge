2c2bd044130c6837697467e138b5f82ed4630133
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-07-04 21:36:15 +0300

    Fixed login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -23,7 +23,7 @@ function App() {
       
       <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login/>}/>
+        <Route path="/login" element={<Login/>}/>
         <Route path="/verifyaccount" element={<AccountVerification/>}/>
       </Routes>
       </BrowserRouter>
