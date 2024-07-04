9b36bb18f5147ff1873de16d19abd20a3e57c122
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-07-04 18:07:53 +0300

    Created custom theme and added favicons

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,25 @@
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import './App.css'
+import { ColorModeContext, useMode } from './theme'
 
 function App() {
+
+  const [theme, colorMode] = useMode();
   
   return (
     <>
-     
+    
+    {/*An error that's just a pain really, works fine*/}
+    {/* @ts-ignore */}
+    <ColorModeContext.Provider value={colorMode}>
+
+    {/*An error that's just a pain really, works fine*/}
+    {/* @ts-ignore */}
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+    </ThemeProvider>
+    </ColorModeContext.Provider>
+    
     </>
   )
 }
