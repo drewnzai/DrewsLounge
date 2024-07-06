3bb158c8b2ae68a78197714d599f50a85b64de97
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-07-06 18:11:10 +0300

    Uninstalled avatar package

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,8 +1,53 @@
+import "react-pro-sidebar/dist/css/styles.css";
+import { Box, useTheme } from "@mui/material";
+import { tokens } from "../theme";
+import { Menu, MenuItem, ProSidebar } from "react-pro-sidebar";
+
+const Conversation = ({conversationName}: {conversationName: string}) => {
+    
+    return(
+    <MenuItem style={{
+        width: "fit-content",
+        alignItems: "center"
+    }}
+    >
+        
+    </MenuItem>);
+};
+
 export default function Sidebar(){
+    
+    const theme = useTheme();
+    const colours = tokens(theme.palette.mode);
 
     return(
-        <>
-        <h1>Sidebar</h1>
-        </>
+        <Box
+        sx={{
+            "& .pro-sidebar-inner": {
+              background: `${colours.primary[400]} !important`,
+            },
+            "& .pro-icon-wrapper": {
+              backgroundColor: "transparent !important",
+            },
+            "& .pro-inner-item": {
+              padding: "5px 35px 5px 20px !important",
+            },
+            "& .pro-inner-item:hover": {
+              color: "#868dfb !important",
+            },
+            "& .pro-menu-item.active": {
+              color: "#6870fa !important",
+            },
+          }}
+        >
+        <ProSidebar collapsed={true}
+        collapsedWidth={"80px"}
+        >
+            <Menu>
+                <Conversation conversationName="user2"/>
+            </Menu>
+        </ProSidebar>
+
+        </Box>
     );
 }
