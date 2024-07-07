ee045fa5cdcb3c1b75dc5285f983911d3c61b503
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-07-07 17:20:02 +0300

    Created MessageDTO

---
+++ backend/src/main/java/com/andrewnzai/DrewsLounge/dtos/MessageDto.java
@@ -0,0 +1,14 @@
+package com.andrewnzai.DrewsLounge.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageDto {
+    private String sender;
+    private String content;
+    private String conversationName;
+}
