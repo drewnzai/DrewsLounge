a7af3c07d57df2c04ff232ab650e7eb7b1a449c1
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-07-09 15:25:47 +0300

    Added JWT authentication

--- backend/src/main/java/com/andrewnzai/DrewsLounge/config/WebSocketConfig.java
+++ backend/src/main/java/com/andrewnzai/DrewsLounge/config/WebSocketConfig.java
@@ -1,14 +1,39 @@
 package com.andrewnzai.DrewsLounge.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import com.andrewnzai.DrewsLounge.services.UserDetailsServiceImpl;
+import com.andrewnzai.DrewsLounge.utils.JwtUtil;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 @Configuration
+@AllArgsConstructor
+@Order(Ordered.HIGHEST_PRECEDENCE + 99)
 @EnableWebSocketMessageBroker
+@Slf4j
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
+
+    private final JwtUtil jwtUtil;
+    private UserDetailsServiceImpl userDetailsService;
+
      public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:5173").withSockJS();
     }
@@ -17,5 +42,36 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor =
+                        MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+                log.info("Headers: {}", accessor);
+
+                assert accessor != null;
+                if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+
+                    String authorizationHeader = accessor.getFirstNativeHeader("Authorization");
+                    assert authorizationHeader != null;
+                    String token = authorizationHeader.substring(7);
+
+                    String username = jwtUtil.getUsernameFromJwtToken(token);
+                    UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                    UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = 
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+
+                    accessor.setUser(usernamePasswordAuthenticationToken);
+                }
+
+                return message;
+            }
+
+        });
     }
 }
