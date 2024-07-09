9a63d93105de8cfb7464c877b308bcc3fc45c987
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-07-09 17:07:56 +0300

    Created conversationDto object

---
+++ backend/src/main/java/com/andrewnzai/DrewsLounge/dtos/ConversationDto.java
@@ -0,0 +1,12 @@
+package com.andrewnzai.DrewsLounge.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ConversationDto {
+    private String conversationName;
+}
