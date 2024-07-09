4dbed2af4151926c990b64f819adf8a554c7d37d
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-07-09 11:44:15 +0300

    Model to show ownership of a group

---
+++ backend/src/main/java/com/andrewnzai/DrewsLounge/models/GroupAdmin.java
@@ -0,0 +1,32 @@
+package com.andrewnzai.DrewsLounge.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "owner")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GroupAdmin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "conversation_id")
+    private Conversation conversation;
+
+}
