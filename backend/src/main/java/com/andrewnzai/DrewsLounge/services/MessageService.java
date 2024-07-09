775c726f557fb01a8ed7baa23ee2278cb388584a
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-07-09 16:08:06 +0300

    Send message functionality

--- backend/src/main/java/com/andrewnzai/DrewsLounge/services/MessageService.java
+++ backend/src/main/java/com/andrewnzai/DrewsLounge/services/MessageService.java
@@ -1,7 +1,13 @@
 package com.andrewnzai.DrewsLounge.services;
 
+import java.time.Instant;
+
 import org.springframework.stereotype.Service;
 
+import com.andrewnzai.DrewsLounge.dtos.MessageDto;
+import com.andrewnzai.DrewsLounge.models.Conversation;
+import com.andrewnzai.DrewsLounge.models.Message;
+import com.andrewnzai.DrewsLounge.repositories.ConversationRepository;
 import com.andrewnzai.DrewsLounge.repositories.MessageRepository;
 
 import lombok.AllArgsConstructor;
@@ -11,8 +17,25 @@ import lombok.AllArgsConstructor;
 public class MessageService {
 
     private final MessageRepository messageRepository;
+    private final ConversationRepository conversationRepository;
 
-    public void sendMessage(MessageDTO messageDTO){
-        
+    private final AuthService authService;
+
+    public void sendMessage(MessageDto messageDto) throws Exception{
+        if(conversationRepository.existsByName(messageDto.getConversationName())){
+            Conversation conversation = conversationRepository.findByName(messageDto.getConversationName());
+
+            Message message = new Message();
+            message.setConversation(conversation);
+            message.setContent(messageDto.getContent());
+            message.setSender(authService.getCurrentUser());
+            message.setSentAt(Instant.now());
+
+            messageRepository.save(message);
+
+        }
+        else{
+            throw new Exception("Conversation does not exist");
+        }
     }
 }
