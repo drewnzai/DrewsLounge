82e3687a732dd3c717bf9033471f153dbb3e6a34
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-07-09 16:31:18 +0300

    Added check for whether sender is in the current conversation

--- backend/src/main/java/com/andrewnzai/DrewsLounge/services/MessageService.java
+++ backend/src/main/java/com/andrewnzai/DrewsLounge/services/MessageService.java
@@ -8,8 +8,10 @@ import org.springframework.stereotype.Service;
 import com.andrewnzai.DrewsLounge.dtos.MessageDto;
 import com.andrewnzai.DrewsLounge.models.Conversation;
 import com.andrewnzai.DrewsLounge.models.Message;
+import com.andrewnzai.DrewsLounge.models.User;
 import com.andrewnzai.DrewsLounge.repositories.ConversationRepository;
 import com.andrewnzai.DrewsLounge.repositories.MessageRepository;
+import com.andrewnzai.DrewsLounge.repositories.UserConversationRepository;
 
 import lombok.AllArgsConstructor;
 
@@ -19,6 +21,7 @@ public class MessageService {
 
     private final MessageRepository messageRepository;
     private final ConversationRepository conversationRepository;
+    private final UserConversationRepository userConversationRepository;
     private final SimpMessagingTemplate simpMessagingTemplate;
 
     private final AuthService authService;
@@ -27,16 +30,23 @@ public class MessageService {
         if(conversationRepository.existsByName(messageDto.getConversationName())){
             
             Conversation conversation = conversationRepository.findByName(messageDto.getConversationName());
-            
-            Message message = new Message();
-            message.setConversation(conversation);
-            message.setContent(messageDto.getContent());
-            message.setSender(authService.getCurrentUser());
-            message.setSentAt(Instant.now());
-            
-            simpMessagingTemplate.convertAndSend("/topic/conversation/" + messageDto.getConversationName(), message);
+            User user = authService.getCurrentUser();
 
-            messageRepository.save(message);
+            if(userConversationRepository.existsByUserAndConversation(user, conversation)){
+
+                Message message = new Message();
+                message.setConversation(conversation);
+                message.setContent(messageDto.getContent());
+                message.setSender(user);
+                message.setSentAt(Instant.now());
+                
+                simpMessagingTemplate.convertAndSend("/topic/conversation/" + messageDto.getConversationName(), message);
+    
+                messageRepository.save(message);
+            }
+            else{
+                throw new Exception("You are not a member of this conversation");
+            }
         }
         else{
             throw new Exception("Conversation does not exist");
