881423f3f957034e64c776232d4bb7100dcbfed2
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-07-09 17:47:09 +0300

    Updated entrypoint to get conversations from backend

--- frontend/src/pages/Entrypoint.tsx
+++ frontend/src/pages/Entrypoint.tsx
@@ -2,15 +2,22 @@ import { Navigate, Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 import { useEffect, useState } from "react";
 import { Conversation } from "../models/Conversation";
+import ConversationService from "../services/ConversationService.service";
 
 export default function Entrypoint(){
     
     const user: any | null = localStorage.getItem("user");
+    const conversationService = new ConversationService();
     const [conversations, setConversations] = useState<Conversation[]>([]);
 
     useEffect(
         () => {
-
+            conversationService.getAllConversations()
+            .then(
+                (response: Conversation[]) => {
+                    setConversations(response);
+                }
+            )
         }, []
     );
 
