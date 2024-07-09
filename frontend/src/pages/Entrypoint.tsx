ecc9132dca9e10d5a9e7c4b5803d7ef06dde960e
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-07-09 17:39:13 +0300

    Added conversations to sidebar through entrypoint

--- frontend/src/pages/Entrypoint.tsx
+++ frontend/src/pages/Entrypoint.tsx
@@ -1,14 +1,23 @@
 import { Navigate, Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
+import { useEffect, useState } from "react";
+import { Conversation } from "../models/Conversation";
 
 export default function Entrypoint(){
     
     const user: any | null = localStorage.getItem("user");
+    const [conversations, setConversations] = useState<Conversation[]>([]);
+
+    useEffect(
+        () => {
+
+        }, []
+    );
 
     return(
        user? <div className="app">
                 
-                <Sidebar/>
+                <Sidebar conversations={conversations}/>
 
             <main className="content">
             <Outlet/>
