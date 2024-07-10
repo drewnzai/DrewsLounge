ce27b901529565fa5b08cb2b5574b5730d5273dd
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-07-10 18:27:27 +0300

    Added TO-Dos

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -4,15 +4,22 @@ import SockJS from 'sockjs-client';
 import axios from 'axios';
 import { useLocation } from 'react-router-dom';
 import { Conversation } from '../models/Conversation';
+import { Message } from '../models/Message';
+import AuthService from '../services/AuthService.service';
+import ConversationService from '../services/ConversationService.service';
 
 
 const Chat = () => {
-    const [messages, setMessages] = useState([]);
-    const [message, setMessage] = useState('');
+    const [message, setMessage] = useState<Message | null>(null);
     const [stompClient, setStompClient] = useState<CompatClient| null>(null);
     const location = useLocation();
     const conversation: Conversation = location.state;
+    const username = "";
+    const authService = new AuthService();
+    const conversationService = new ConversationService();
 
+    // TO-DO change the backend to render full conversation name and modify it from here
+    // Do the message functionality
     useEffect(() => {
         const socket = new SockJS('http://localhost:8080/ws');
         const client = Stomp.over(socket);
@@ -37,7 +44,7 @@ const Chat = () => {
 
     return (
         <div>
-            <input type="text" value={message} onChange={(e) => setMessage(e.target.value)} />
+            <input type="text"/>
             <button onClick={sendMessage}>Send</button>
         </div>
     );
