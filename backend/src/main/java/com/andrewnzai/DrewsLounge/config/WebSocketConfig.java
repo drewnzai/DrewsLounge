bb6772450ff89bc3d0ba34e5feba828c240d2c12
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-07-13 14:30:29 +0300

    Wasn't working and I'm already tired, reverted to best commit

--- backend/src/main/java/com/andrewnzai/DrewsLounge/config/WebSocketConfig.java
+++ backend/src/main/java/com/andrewnzai/DrewsLounge/config/WebSocketConfig.java
@@ -1,26 +1,10 @@
 package com.andrewnzai.DrewsLounge.config;
 
-import com.andrewnzai.DrewsLounge.services.UserDetailsServiceImpl;
-import com.andrewnzai.DrewsLounge.utils.JwtUtil;
-import com.andrewnzai.DrewsLounge.websocket.WebSocketHandShakeInterceptor;
-
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
-import org.springframework.messaging.support.ChannelInterceptor;
-import org.springframework.messaging.support.MessageHeaderAccessor;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -29,15 +13,10 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @AllArgsConstructor
 @Order(Ordered.HIGHEST_PRECEDENCE + 99)
 @EnableWebSocketMessageBroker
-@Slf4j
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
-
-    private final JwtUtil jwtUtil;
-    private UserDetailsServiceImpl userDetailsService;
 
      public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-            .addInterceptors(webSocketHandShakeInterceptor())
             .setAllowedOrigins("http://localhost:5173").withSockJS();
     }
 
@@ -46,41 +25,4 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
         registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
     }
-
-    @Override
-    public void configureClientInboundChannel(ChannelRegistration registration) {
-        registration.interceptors(new ChannelInterceptor() {
-            @Override
-            public Message<?> preSend(Message<?> message, MessageChannel channel) {
-                StompHeaderAccessor accessor =
-                        MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
-                log.info("Headers: {}", accessor);
-
-                if(accessor != null){
-                    String authorizationHeader = accessor.getFirstNativeHeader("Authorization");                    
-                    
-                    if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
-                        
-                        String token = authorizationHeader.substring(7);
-    
-                        String username = jwtUtil.getUsernameFromJwtToken(token);
-                        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-                        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                        SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
-    
-                        accessor.setUser(usernamePasswordAuthenticationToken);
-                    }
-                }
-
-                return message;
-            }
-
-        });
-    }
-
-    @Bean
-    public WebSocketHandShakeInterceptor webSocketHandShakeInterceptor(){
-        return new WebSocketHandShakeInterceptor();
-    }
-
 }
