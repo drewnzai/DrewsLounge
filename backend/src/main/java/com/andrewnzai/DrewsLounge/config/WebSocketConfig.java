8a792d8cbf36f73d81554aa381215ac25283dec7
Andrew Nzai <andrewnzaikombe@gmail.com>
2024-07-13 11:23:30 +0300

    Provide JWT authentication for websocket connections

--- backend/src/main/java/com/andrewnzai/DrewsLounge/config/WebSocketConfig.java
+++ backend/src/main/java/com/andrewnzai/DrewsLounge/config/WebSocketConfig.java
@@ -7,7 +7,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -30,6 +40,36 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor =
+                        MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+                log.info("Headers: {}", accessor);
+
+                assert accessor != null;
+                if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+
+                    String authorizationHeader = accessor.getFirstNativeHeader("Authorization");
+                    assert authorizationHeader != null;
+                    String token = authorizationHeader.substring(7);
+
+                    String username = jwtUtil.getUsernameFromJwtToken(token);
+                    UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                    UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+
+                    accessor.setUser(usernamePasswordAuthenticationToken);
+                }
+
+                return message;
+            }
+
+        });
     }
 
 }
